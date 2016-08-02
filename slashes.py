b3438ef2bcd93bccdcfdd985bb6af739051ef288
Jakob <jakob.kogler@gmail.com>
2016-08-02 21:01:55 +0200

    Support custom print function

--- slashes.py
+++ slashes.py
@@ -1,83 +1,88 @@
 import sys
 
 
-def execute_code(code, debug=False):
-	mode = 0
-	pat = ''
-	sub = ''
-	tex = ''
-	
-	while code:
-		c, *code = code
-		if mode == 0:
-			if c == '/':
-				mode += 1
-			elif c == '\\':
-				if code:
-					c, *code = code
-					print(c, end='')
-			else:
-				print(c, end='')
+def execute_code(code, debug=False, print_func=None):
+    if not print_func:
+        def myPrint(x):
+            print(x, end='')
+        print_func = myPrint        
 
-		elif mode == 1:
-			if c == '/':
-				mode += 1
-			elif c == '\\':
-				if code:
-					c, *code = code
-					pat += c
-			else:
-				pat += c
+    mode = 0
+    pat = ''
+    sub = ''
+    tex = ''
+    
+    while code:
+        c, *code = code
+        if mode == 0:
+            if c == '/':
+                mode += 1
+            elif c == '\\':
+                if code:
+                    c, *code = code
+                    print_func(c)
+            else:
+                print_func(c)
 
-		elif mode == 2:
-			if c == '/':
-				mode += 1
-			elif c == '\\':
-				if code:
-					c, *code = code
-					sub += c
-			else:
-				sub += c
+        elif mode == 1:
+            if c == '/':
+                mode += 1
+            elif c == '\\':
+                if code:
+                    c, *code = code
+                    pat += c
+            else:
+                pat += c
 
-		elif mode == 3:
-			tex += c
+        elif mode == 2:
+            if c == '/':
+                mode += 1
+            elif c == '\\':
+                if code:
+                    c, *code = code
+                    sub += c
+            else:
+                sub += c
 
-	
-	if mode == 3:
-		while pat in tex:
-			idx = tex.index(pat)
-			tex = list(tex)
-			sub = list(sub)
+        elif mode == 3:
+            tex += c
 
-			tex[idx:idx+len(pat)] = sub
-			tex = ''.join(tex)
+    
+    if mode == 3:
+        while pat in tex:
+            idx = tex.index(pat)
+            tex = list(tex)
+            sub = list(sub)
 
-		if tex:
-			if debug:
-				print('\n-----------')
-				print(tex)
-				print('\n-----------')
+            tex[idx:idx+len(pat)] = sub
+            tex = ''.join(tex)
 
-			execute_code(tex, debug)
+        if tex:
+            if debug:
+                print('\n-----------')
+                print(tex)
+                print('\n-----------')
+
+            execute_code(tex, debug, print_func)
 
 
 
 def main():
-	if len(sys.argv) == 1:
-		return
+    if len(sys.argv) == 1:
+        return
 
-	code = ''
-	debug = False
+    code = ''
+    debug = False
 
-	with open(sys.argv[1]) as file:
-		code = '\n'.join(line.rstrip('\n') for line in file)
+    with open(sys.argv[1]) as file:
+        code = '\n'.join(line.rstrip('\n') for line in file)
 
-	if len(sys.argv) > 2 and sys.argv[2] == '-d':
-		debug = True
+    if len(sys.argv) > 2 and sys.argv[2] == '-d':
+        debug = True
 
-	execute_code(code, debug)
+    execute_code(code, debug)
 
-	
+    
 
 if __name__ == '__main__':
-	main()
+    main()
